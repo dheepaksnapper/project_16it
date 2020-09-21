22fcf221e52bd091b93ed629a65e769465a39c89
Dheepak <16it10is@gmail.com>
2020-09-21 11:24:06 +0530

    initial commit from remote:

---
+++ script/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    console.log('document loaded');
+    var ctx = document.getElementById('tempChart').getContext('2d');
+    var tempChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+    });
+    var ctx1 = document.getElementById('ammoChart').getContext('2d');
+    var ammoChart = new Chart(ctx1, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+    });
+});
