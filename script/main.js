4e726c33ab125e31cd1786f91e879958dd6ae091
Dheepak <16it10is@gmail.com>
2020-09-21 12:36:39 +0530

    added  firebase and chart function to main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,24 @@
-$(document).ready(function () {
-    console.log('document loaded');
+var tempChart,ammoChart;
+
+
+function initializeFirebase() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBB_pWCk4PpS2rKEd7i3t2rzuydGiX06Yg",
+        authDomain: "project-16it.firebaseapp.com",
+        databaseURL: "https://project-16it.firebaseio.com",
+        projectId: "project-16it",
+        storageBucket: "project-16it.appspot.com",
+        messagingSenderId: "916528025552",
+        appId: "1:916528025552:web:fbc991288b388b7ecaf3d5",
+        measurementId: "G-94J7ENHRF4"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+}
+
+function initializeCharts() {
     var ctx = document.getElementById('tempChart').getContext('2d');
-    var tempChart = new Chart(ctx, {
+    tempChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -22,7 +39,7 @@ $(document).ready(function () {
     }
     });
     var ctx1 = document.getElementById('ammoChart').getContext('2d');
-    var ammoChart = new Chart(ctx1, {
+    ammoChart = new Chart(ctx1, {
     type: 'line',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -42,4 +59,13 @@ $(document).ready(function () {
         }
     }
     });
+
+}
+
+$(document).ready(function () {
+    console.log('document loaded');
+    initializeFirebase();
+    initializeCharts();
 });
+
+
